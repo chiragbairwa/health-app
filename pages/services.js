5151f32c1b73b5f6425f4fbf62571ca1a1ef09b8
chirag <chirag.dev18@gmail.com>
2021-12-15 20:21:48 +0530

    Homepage

---
+++ pages/services.js
@@ -0,0 +1,27 @@
+export default function Services() {
+  return (
+    <div className="services">
+      <h2>Diets plans we provide</h2>
+      <div>
+        <div className="card">
+          <h2>Basic</h2>
+          <span>1500kcal</span>
+
+          <p align="right">From $29/Month</p>
+        </div>
+        <div className="card">
+          <h2>Intermediate</h2>
+          <span>1500kcal</span>
+
+          <p align="right">From $29/Month</p>
+        </div>
+        <div className="card">
+          <h2>Advanced</h2>
+          <span>3500kcal</span>
+
+          <p align="right">From $29/Month</p>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ pages/welcome.js
@@ -0,0 +1,24 @@
+//import Styles from '../styles/navbar.css'
+
+export default function Welcome() {
+  return (
+    <div className="welcome">
+      <pre>
+        {`
+Your Diet doesn't
+have to be Boring`}
+      </pre>
+      <p>
+        With our catering you can spend more time with your family and also
+        level up your standard of living. Try our healthy diet content.
+      </p>
+
+      <a href="#" className="welcome-btn1 btn">
+        Try Free Trial
+      </a>
+      <a href="#" className="welcome-btn2 btn">
+        Contact Us
+      </a>
+    </div>
+  )
+}
