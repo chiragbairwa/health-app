4d90ff15985981756855c486e84dee02eec8b46d
chirag <chirag.dev18@gmail.com>
2021-12-23 19:18:02 +0530

    Plan Completed.

--- pages/plans.js
+++ pages/plans.js
@@ -8,21 +8,42 @@ export default function Plans() {
       {/* Cards div */}
       <div>
         <div className="plan-card">
-          <h2>Monthly</h2>
-          <span>1500kcal</span>
-          <p align="right">From $29/Month</p>
+          <h2 align="top">Monthly</h2>
+          <ul className="plan-perks">
+            <li>Customised 4 Weeks Diet Plans.</li>
+            <li>4 Checkout Calls.</li>
+            <li>Indian Healthy Recipes.</li>
+            <li>Whatsapp Support.</li>
+            <li className="plan-perks-blank">Monthly Excercise Plan.</li>
+            <li className="plan-perks-blank">Workout Guides & Many More.</li>
+          </ul>
+          <p>From $150/-</p>
         </div>
 
         <div className="plan-card">
-          <h2>Quarterly</h2>
-          <span>1500kcal</span>
-          <p align="right">From $29/Month</p>
+          <h2 align="top">Quarterly</h2>
+          <ul className="plan-perks">
+            <li>Customised 3 Months Diet Plans.</li>
+            <li>12 Checkout Calls.</li>
+            <li>Indian Healthy Recipes.</li>
+            <li>Whatsapp Support.</li>
+            <li>Monthly Excercise Plan.</li>
+            <li className="plan-perks-blank">Workout Guides & Many More.</li>
+          </ul>
+          <p>From $450/-</p>
         </div>
 
         <div className="plan-card">
-          <h2>Half-Year</h2>
-          <span>3500kcal</span>
-          <p align="right">From $29/Month</p>
+          <h2 align="top">Half-Year</h2>
+          <ul className="plan-perks">
+            <li>Customised 6 Months Diet Plans.</li>
+            <li>24 Checkout Calls.</li>
+            <li>Indian Healthy Recipes.</li>
+            <li>Unlimited Whatsapp Support.</li>
+            <li>Monthly Excercise Plan.</li>
+            <li>Workout Guides & Many More.</li>
+          </ul>
+          <p>From $850/-</p>
         </div>
       </div>
 
