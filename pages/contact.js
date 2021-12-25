e5f3d82a170833922ca6fcf97f41d26acc68354b
chirag <chirag.dev18@gmail.com>
2021-12-25 22:06:48 +0530

    Contact Details

--- pages/contact.js
+++ pages/contact.js
@@ -7,11 +7,19 @@ export default function Contact() {
         <div className="contact-card">
           <div className="contact-img"></div>
 
-          <div className="contact-detail"></div>
+          <div className="contact-detail">
+            <h2>Contact Us</h2>
+            <p>{'Mobile:    9876543210'}</p>
+            <p>{'Email :    abc@gmail.com'}</p>
+
+            <button>Book Now</button>
+          </div>
         </div>
       </div>
 
-      <div className="contact-bg"></div>
+      <div className="contact-bg">
+        <p>©️ 2021 ABC Company, All Rights Reserved</p>
+      </div>
     </>
   )
 }
